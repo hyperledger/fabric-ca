90bd09fe6aaa5a2a43b3e5f800a17257e5a834ed
Keith Smith <bksmith@us.ibm.com>
2016-12-02 13:53:40 -0500

    Copy/modify cfssl serve.go
    This changeset doesn't change anything functionally.
    I simply copied the cfssl/cli/serve.go file and integrated it into
    fabric-cop/cli/server.go.
    This gives us control to make several future changes including:
    1) Implement our own DB Accessor interface for the certificates table
       so that we can:
       a) extend the schema with a column for the enrollent ID so we can
          revoke all certs for a particular user.  This is needed for
          FAB-116 in the short term.
       b) in the future, potentially provide a nosql implementation of
          the interface.
    2) Plug in BCCSP calls (Vlad is working on this)
    3) I made some simplications related to adding new endpoints since the
       endpoints list was in serve.go
    
    https://jira.hyperledger.org/browse/FAB-116
    
    Change-Id: I243e508eebc23297fcbd440b183ef6d883109f12
    Signed-off-by: Keith Smith <bksmith@us.ibm.com>

--- cli/server/server.go
+++ cli/server/server.go
@@ -17,20 +17,48 @@ limitations under the License.
 package server
 
 import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 	"sync"
 
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/cloudflare/cfssl/api"
+	"github.com/cloudflare/cfssl/api/bundle"
+	"github.com/cloudflare/cfssl/api/certinfo"
+	"github.com/cloudflare/cfssl/api/crl"
+	"github.com/cloudflare/cfssl/api/generator"
+	"github.com/cloudflare/cfssl/api/info"
+	"github.com/cloudflare/cfssl/api/initca"
+	apiocsp "github.com/cloudflare/cfssl/api/ocsp"
+	"github.com/cloudflare/cfssl/api/scan"
+	"github.com/cloudflare/cfssl/api/signhandler"
+	"github.com/cloudflare/cfssl/bundler"
+	"github.com/cloudflare/cfssl/certdb/dbconf"
 	"github.com/cloudflare/cfssl/certdb/sql"
 	"github.com/cloudflare/cfssl/cli"
-	"github.com/cloudflare/cfssl/cli/serve"
+	"github.com/cloudflare/cfssl/cli/ocspsign"
 	"github.com/cloudflare/cfssl/cli/sign"
+	"github.com/cloudflare/cfssl/helpers"
 	"github.com/cloudflare/cfssl/log"
+	"github.com/cloudflare/cfssl/ocsp"
+	"github.com/cloudflare/cfssl/signer"
+	"github.com/cloudflare/cfssl/ubiquity"
 	cop "github.com/hyperledger/fabric-cop/api"
+	"github.com/jmoiron/sqlx"
 )
 
-// Usage text of 'cfssl serve'
-var serverUsageText = `cop server start -- start the COP server
+var (
+	// Usage text of 'cop server start'
+	serverUsageText = `cop server start -- start the COP server
 
 Usage:
         cop server start [-address address] [-ca cert] [-ca-bundle bundle] \
@@ -44,38 +72,40 @@ Usage:
 Flags:
 `
 
-// Flags used by 'cfssl serve'
-var serverFlags = []string{"address", "port", "ca", "ca-key", "ca-bundle", "int-bundle", "int-dir", "metadata",
-	"remote", "config", "responder", "responder-key", "tls-key", "tls-cert", "mutual-tls-ca", "mutual-tls-cn",
-	"tls-remote-ca", "mutual-tls-client-cert", "mutual-tls-client-key", "db-config"}
+	// Flags used by 'cfssl serve'
+	serverFlags = []string{"address", "port", "ca", "ca-key", "ca-bundle", "int-bundle", "int-dir", "metadata",
+		"remote", "config", "responder", "responder-key", "tls-key", "tls-cert", "mutual-tls-ca", "mutual-tls-cn",
+		"tls-remote-ca", "mutual-tls-client-cert", "mutual-tls-client-key", "db-config"}
 
-var (
-	mutex = &sync.RWMutex{}
+	// V1APIPrefix is the prefix of all CFSSL V1 API Endpoints.
+	V1APIPrefix = "/api/v1/cfssl/"
 )
 
-// Server ...
-type Server struct {
-}
+var (
+	conf       cli.Config
+	s          signer.Signer
+	ocspSigner ocsp.Signer
+	db         *sqlx.DB
+	mutex      = &sync.RWMutex{}
+)
 
-// Command defines the command will start the server and registers endpoints
+var (
+	errBadSigner          = errors.New("signer not initialized")
+	errNoCertDBConfigured = errors.New("cert db not configured (missing -db-config)")
+)
+
+// Command defines the server-related commands and calls cli.Start to process args
 func Command() error {
 	// The server commands
 	cmds := map[string]*cli.Command{
 		"init":  InitServerCommand,
 		"start": StartCommand,
 	}
-	// Set the authentication handler
-	serve.SetWrapHandler(NewAuthWrapper)
-	// Add the "register" route/endpoint
-	serve.SetEndpoint("register", NewRegisterHandler)
-	// Add the "enroll" route/endpoint
-	serve.SetEndpoint("enroll", NewEnrollHandler)
-	// Add the "reenroll" route/endpoint
-	serve.SetEndpoint("reenroll", NewReenrollHandler)
-	// Add the "revoke" route/endpoint
-	serve.SetEndpoint("revoke", NewRevokeHandler)
-
 	return cli.Start(cmds)
+}
+
+// Server ...
+type Server struct {
 }
 
 // CreateHome will create a home directory if it does not exist
@@ -105,16 +135,16 @@ func (s *Server) CreateHome() (string, error) {
 
 // BootstrapDB loads the database based on config file
 func (s *Server) BootstrapDB(cfg *Config) error {
-	log.Debug("Bootstrap DB")
+	log.Debug("Entering BootstrapDB ...")
 	b := BootstrapDB()
 	b.PopulateGroupsTable()
 	b.PopulateUsersTable()
-
+	log.Debug("Completed BootstrapDB")
 	return nil
 }
 
-// startMain is the command line entry point to the API server. It sets up a
-// new HTTP server to handle sign, bundle, and validate requests.
+// startMain is the command line entry point to the COP server.
+// It sets up a new HTTP server to handle COP requests.
 func startMain(args []string, c cli.Config) error {
 	log.Debug("server.startMain")
 
@@ -133,7 +163,7 @@ func startMain(args []string, c cli.Config) error {
 		return cop.NewError(cop.DatabaseError, msg)
 	}
 
-	db, err := GetDB(cfg)
+	db, err = GetDB(cfg)
 	if err != nil {
 		log.Error("Failed to open database")
 		return err
@@ -156,10 +186,242 @@ func startMain(args []string, c cli.Config) error {
 
 	s.BootstrapDB(cfg)
 
-	return serve.Command.Main(args, c)
+	return serverMain(args, c)
 }
 
-// Start will start server - only used for test case execution
+// serverMain is the command line entry point to the API server. It sets up a
+// new HTTP server to handle all endpoints
+func serverMain(args []string, c cli.Config) error {
+	conf = c
+	// serve doesn't support arguments.
+	if len(args) > 0 {
+		return errors.New("argument is provided but not defined; please refer to the usage by flag -h")
+	}
+
+	bundler.IntermediateStash = conf.IntDir
+	var err error
+
+	if err = ubiquity.LoadPlatforms(conf.Metadata); err != nil {
+		return err
+	}
+
+	if c.DBConfigFile != "" {
+		db, err = dbconf.DBFromConfig(c.DBConfigFile)
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Info("Initializing signer")
+
+	if s, err = sign.SignerFromConfigAndDB(c, db); err != nil {
+		log.Warningf("couldn't initialize signer: %v", err)
+	}
+
+	if ocspSigner, err = ocspsign.SignerFromConfig(c); err != nil {
+		log.Warningf("couldn't initialize ocsp signer: %v", err)
+	}
+
+	registerHandlers()
+
+	addr := net.JoinHostPort(conf.Address, strconv.Itoa(conf.Port))
+
+	if conf.TLSCertFile == "" || conf.TLSKeyFile == "" {
+		log.Info("Now listening on ", addr)
+		return http.ListenAndServe(addr, nil)
+	}
+	if conf.MutualTLSCAFile != "" {
+		clientPool, err := helpers.LoadPEMCertPool(conf.MutualTLSCAFile)
+		if err != nil {
+			return fmt.Errorf("failed to load mutual TLS CA file: %s", err)
+		}
+
+		server := http.Server{
+			Addr: addr,
+			TLSConfig: &tls.Config{
+				ClientAuth: tls.RequireAndVerifyClientCert,
+				ClientCAs:  clientPool,
+			},
+		}
+
+		if conf.MutualTLSCNRegex != "" {
+			log.Debugf(`Requiring CN matches regex "%s" for client connections`, conf.MutualTLSCNRegex)
+			re, err := regexp.Compile(conf.MutualTLSCNRegex)
+			if err != nil {
+				return fmt.Errorf("malformed CN regex: %s", err)
+			}
+			server.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r != nil && r.TLS != nil && len(r.TLS.PeerCertificates) > 0 {
+					if re.MatchString(r.TLS.PeerCertificates[0].Subject.CommonName) {
+						http.DefaultServeMux.ServeHTTP(w, r)
+						return
+					}
+					log.Warningf(`Rejected client cert CN "%s" does not match regex %s`,
+						r.TLS.PeerCertificates[0].Subject.CommonName, conf.MutualTLSCNRegex)
+				}
+				http.Error(w, "Invalid CN", http.StatusForbidden)
+			})
+		}
+		log.Info("Now listening with mutual TLS on https://", addr)
+		return server.ListenAndServeTLS(conf.TLSCertFile, conf.TLSKeyFile)
+	}
+	log.Info("Now listening on https://", addr)
+	return http.ListenAndServeTLS(addr, conf.TLSCertFile, conf.TLSKeyFile, nil)
+
+}
+
+// registerHandlers instantiates various handlers and associate them to corresponding endpoints.
+func registerHandlers() {
+	for path, getHandler := range endpoints {
+		log.Debugf("getHandler for %s", path)
+		if handler, err := getHandler(); err != nil {
+			log.Warningf("endpoint '%s' is disabled: %v", path, err)
+		} else {
+			if path, handler, err = NewAuthWrapper(path, handler, err); err != nil {
+				log.Warningf("endpoint '%s' has been disabled: %v", path, err)
+			} else {
+				log.Infof("endpoint '%s' is enabled", path)
+				http.Handle(path, handler)
+			}
+		}
+	}
+	log.Info("Handler set up complete.")
+}
+
+// v1APIPath prepends the V1 API prefix to endpoints not beginning with "/"
+func v1APIPath(path string) string {
+	if !strings.HasPrefix(path, "/") {
+		path = V1APIPrefix + path
+	}
+	return (&url.URL{Path: path}).String()
+}
+
+// httpBox implements http.FileSystem which allows the use of Box with a http.FileServer.
+// Atempting to Open an API endpoint will result in an error.
+type httpBox struct {
+	*rice.Box
+	redirects map[string]string
+}
+
+func (hb *httpBox) findStaticBox() (err error) {
+	hb.Box, err = rice.FindBox("static")
+	return
+}
+
+// Open returns a File for non-API enpoints using the http.File interface.
+func (hb *httpBox) Open(name string) (http.File, error) {
+	if strings.HasPrefix(name, V1APIPrefix) {
+		return nil, os.ErrNotExist
+	}
+
+	if location, ok := hb.redirects[name]; ok {
+		return hb.Box.Open(location)
+	}
+
+	return hb.Box.Open(name)
+}
+
+// staticBox is the box containing all static assets.
+var staticBox = &httpBox{
+	redirects: map[string]string{
+		"/scan":   "/index.html",
+		"/bundle": "/index.html",
+	},
+}
+
+var endpoints = map[string]func() (http.Handler, error){
+
+	// The following are the COP-specific endpoints
+	"register": NewRegisterHandler,
+	"enroll":   NewEnrollHandler,
+	"reenroll": NewReenrollHandler,
+	"revoke":   NewRevokeHandler,
+
+	// The remainder are the CFSSL endpoints
+	"sign": func() (http.Handler, error) {
+		if s == nil {
+			return nil, errBadSigner
+		}
+		return signhandler.NewHandlerFromSigner(s)
+	},
+
+	"authsign": func() (http.Handler, error) {
+		if s == nil {
+			return nil, errBadSigner
+		}
+		return signhandler.NewAuthHandlerFromSigner(s)
+	},
+
+	"info": func() (http.Handler, error) {
+		if s == nil {
+			return nil, errBadSigner
+		}
+		return info.NewHandler(s)
+	},
+
+	"gencrl": func() (http.Handler, error) {
+		if s == nil {
+			return nil, errBadSigner
+		}
+		return crl.NewHandler(), nil
+	},
+
+	"newcert": func() (http.Handler, error) {
+		if s == nil {
+			return nil, errBadSigner
+		}
+		h := generator.NewCertGeneratorHandlerFromSigner(generator.CSRValidate, s)
+		if conf.CABundleFile != "" && conf.IntBundleFile != "" {
+			cg := h.(api.HTTPHandler).Handler.(*generator.CertGeneratorHandler)
+			if err := cg.SetBundler(conf.CABundleFile, conf.IntBundleFile); err != nil {
+				return nil, err
+			}
+		}
+		return h, nil
+	},
+
+	"bundle": func() (http.Handler, error) {
+		return bundle.NewHandler(conf.CABundleFile, conf.IntBundleFile)
+	},
+
+	"newkey": func() (http.Handler, error) {
+		return generator.NewHandler(generator.CSRValidate)
+	},
+
+	"init_ca": func() (http.Handler, error) {
+		return initca.NewHandler(), nil
+	},
+
+	"scan": func() (http.Handler, error) {
+		return scan.NewHandler(conf.CABundleFile)
+	},
+
+	"scaninfo": func() (http.Handler, error) {
+		return scan.NewInfoHandler(), nil
+	},
+
+	"certinfo": func() (http.Handler, error) {
+		return certinfo.NewHandler(), nil
+	},
+
+	"ocspsign": func() (http.Handler, error) {
+		if ocspSigner == nil {
+			return nil, errBadSigner
+		}
+		return apiocsp.NewHandler(ocspSigner), nil
+	},
+
+	"/": func() (http.Handler, error) {
+		if err := staticBox.findStaticBox(); err != nil {
+			return nil, err
+		}
+
+		return http.FileServer(staticBox), nil
+	},
+}
+
+// Start will start server
+// THIS IS ONLY USED FOR TEST CASE EXECUTION
 func Start(dir string) {
 	log.Debug("Server starting")
 	osArgs := os.Args
@@ -171,5 +433,5 @@ func Start(dir string) {
 	os.Args = osArgs
 }
 
-// StartCommand assembles the definition of Command 'serve'
+// StartCommand assembles the definition of Command 'cop server start'
 var StartCommand = &cli.Command{UsageText: serverUsageText, Flags: serverFlags, Main: startMain}
