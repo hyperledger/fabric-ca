hyperledger-gerrit-archive/fabric-cop

Add support for certificate revocation
This is part 1 of supporting certificate revocation.
Certificate revocation consists of the following parts:
1) Revoke a single certificate identified by serial number, adding to CRL list of COP server only.
2) Revoke all certificates associated with an enrollment ID (COP server only) and disable the enrollment ID.
3) Add revoked certificate serial numbers to the ledger's CRL list.
4) Prune the ledger's CRL list for certificates that have expired

https://jira.hyperledger.org/browse/FAB-116

Change-Id: Idab965c05998b447ba004b44b72a8c06b555dabf
Signed-off-by: Keith Smith <bksmith@us.ibm.com>

cli/server/auth.go
@@ -18,7 +18,9 @@ package server
 
 import (
 	"bytes"
+	"encoding/hex"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -111,6 +113,11 @@ func (ah *copAuthHandler) serveHTTP(w http.ResponseWriter, r *http.Request) erro
 			log.Debug("User '%s' not found", user)
 			return cop.NewError(cop.EnrollingUserError, "User '%s' not found", user)
 		}
+		if userRecord.State != 0 {
+			err := fmt.Errorf("User '%s' is in state %d", user, userRecord.State)
+			log.Debug("%s", err)
+			return err
+		}
 		if userRecord.Token != pwd {
 			log.Debug("Incorrect password for '%s'; received %s but expected %s", userRecord.ID, pwd, userRecord.Token)
 			return cop.NewError(cop.EnrollingUserError, "Incorrect password for '%s'; received %s but expected %s", userRecord.ID, pwd, userRecord.Token)
@@ -123,13 +130,31 @@ func (ah *copAuthHandler) serveHTTP(w http.ResponseWriter, r *http.Request) erro
 	}
 	// Perform token verification
 	if ah.token {
+		// read body
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			return authError
 		}
 		r.Body = ioutil.NopCloser(bytes.NewReader(body))
-		_, err2 := util.VerifyToken(authHdr, body)
+		// verify token
+		cert, err2 := util.VerifyToken(authHdr, body)
 		if err2 != nil {
+			return authError
+		}
+		// check status of certificate
+		serial := cert.SerialNumber.String()
+		aki := hex.EncodeToString(cert.AuthorityKeyId)
+		certs, err := CFG.cfsslDBAccessor.GetCertificate(serial, aki)
+		if err != nil {
+			log.Debugf("GetCertificate failed: %s", err)
+			return authError
+		}
+		if len(certs) != 1 {
+			log.Debugf("Expecting 1 certificate but found %d; serial=%s, aki=%s", len(certs), serial, aki)
+			return authError
+		}
+		if certs[0].Status != "good" {
+			log.Debugf("Auth failure - certificate status is %s", certs[0].Status)
 			return authError
 		}
 	}

cli/server/revoke.go
@@ -1,0 +1,105 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+                 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package server
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cloudflare/cfssl/api"
+	"github.com/cloudflare/cfssl/log"
+
+	"github.com/hyperledger/fabric-cop/idp"
+	"github.com/hyperledger/fabric-cop/util"
+)
+
+// NewRevokeHandler is constructor for revoke handler
+func NewRevokeHandler() (h http.Handler, err error) {
+	// NewHandler is constructor for register handler
+	return &api.HTTPHandler{
+		Handler: &revokeHandler{},
+		Methods: []string{"POST"}}, nil
+}
+
+// revokeHandler for revoke requests
+type revokeHandler struct {
+}
+
+// Handle an revoke request
+func (h *revokeHandler) Handle(w http.ResponseWriter, r *http.Request) error {
+
+	log.Debug("Revoke request received")
+
+	authHdr := r.Header.Get("authorization")
+	if authHdr == "" {
+		return authErr(w, errors.New("no authorization header"))
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return badRequest(w, err)
+	}
+	r.Body.Close()
+
+	cert, err := util.VerifyToken(authHdr, body)
+	if err != nil {
+		return authErr(w, err)
+	}
+
+	err = userHasAttribute(cert.Subject.CommonName, "hf.Revoker")
+	if err != nil {
+		return authErr(w, err)
+	}
+
+	// Parse revoke request body
+	var req idp.RevocationRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		return badRequest(w, err)
+	}
+
+	log.Debugf("Revoke request: %+v", req)
+
+	if req.Serial != "" && req.AKI != "" {
+		err = CFG.cfsslDBAccessor.RevokeCertificate(req.Serial, req.AKI, req.Reason)
+		if err != nil {
+			log.Warningf("Revoke failed: %s", err)
+			return notFound(w, err)
+		}
+	} else if req.Name != "" {
+		user, err := CFG.DBAccessor.GetUser(req.Name)
+		if err != nil {
+			log.Warningf("Revoke failed: %s", err)
+			return notFound(w, err)
+		}
+		user.State = -1
+		err = CFG.DBAccessor.UpdateUser(user)
+		if err != nil {
+			log.Warningf("Revoke failed: %s", err)
+			return dbErr(w, err)
+		}
+	} else {
+		return badRequest(w, errors.New("either Name or Serial and AKI are required for a revoke request"))
+	}
+
+	log.Debug("Revoke was successful: %+v", req)
+
+	result := map[string]string{}
+	return api.SendResponse(w, result)
+}

cli/server/server.go
@@ -21,6 +21,7 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/cloudflare/cfssl/certdb/sql"
 	"github.com/cloudflare/cfssl/cli"
 	"github.com/cloudflare/cfssl/cli/serve"
 	"github.com/cloudflare/cfssl/log"
@@ -56,7 +57,7 @@ type Server struct {
 }
 
 // Command defines the command will start the server and registers endpoints
-func Command() {
+func Command() error {
 	// The server commands
 	cmds := map[string]*cli.Command{
 		"init":  InitServerCommand,
@@ -70,12 +71,10 @@ func Command() {
 	serve.SetEndpoint("enroll", NewEnrollHandler)
 	// Add the "reenroll" route/endpoint
 	serve.SetEndpoint("reenroll", NewReenrollHandler)
+	// Add the "revoke" route/endpoint
+	serve.SetEndpoint("revoke", NewRevokeHandler)
 
-	// If the CLI returns an error, exit with an appropriate status code.
-	err := cli.Start(cmds)
-	if err != nil {
-		os.Exit(1)
-	}
+	return cli.Start(cmds)
 }
 
 // CreateHome will create a home directory if it does not exist
@@ -142,6 +141,7 @@ func startMain(args []string, c cli.Config) error {
 	cfg.DB = db
 	cfg.DBAccessor = NewDBAccessor()
 	cfg.DBAccessor.SetDB(db)
+	cfg.cfsslDBAccessor = sql.NewAccessor(db)
 
 	s.BootstrapDB(cfg)
 

hyperledger/fabric-ca

cli/server/auth.go
@@ -18,7 +18,9 @@ package server
 
 import (
 	"bytes"
+	"encoding/hex"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -111,6 +113,11 @@ func (ah *copAuthHandler) serveHTTP(w http.ResponseWriter, r *http.Request) erro
 			log.Debug("User '%s' not found", user)
 			return cop.NewError(cop.EnrollingUserError, "User '%s' not found", user)
 		}
+		if userRecord.State != 0 {
+			err := fmt.Errorf("User '%s' is in state %d", user, userRecord.State)
+			log.Debug("%s", err)
+			return err
+		}
 		if userRecord.Token != pwd {
 			log.Debug("Incorrect password for '%s'; received %s but expected %s", userRecord.ID, pwd, userRecord.Token)
 			return cop.NewError(cop.EnrollingUserError, "Incorrect password for '%s'; received %s but expected %s", userRecord.ID, pwd, userRecord.Token)
@@ -123,13 +130,31 @@ func (ah *copAuthHandler) serveHTTP(w http.ResponseWriter, r *http.Request) erro
 	}
 	// Perform token verification
 	if ah.token {
+		// read body
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			return authError
 		}
 		r.Body = ioutil.NopCloser(bytes.NewReader(body))
-		_, err2 := util.VerifyToken(authHdr, body)
+		// verify token
+		cert, err2 := util.VerifyToken(authHdr, body)
 		if err2 != nil {
+			return authError
+		}
+		// check status of certificate
+		serial := cert.SerialNumber.String()
+		aki := hex.EncodeToString(cert.AuthorityKeyId)
+		certs, err := CFG.certDBAccessor.GetCertificate(serial, aki)
+		if err != nil {
+			log.Debugf("GetCertificate failed: %s", err)
+			return authError
+		}
+		if len(certs) != 1 {
+			log.Debugf("Expecting 1 certificate but found %d; serial=%s, aki=%s", len(certs), serial, aki)
+			return authError
+		}
+		if certs[0].Status != "good" {
+			log.Debugf("Auth failure - certificate status is %s", certs[0].Status)
 			return authError
 		}
 	}

cli/server/revoke.go
@@ -1,0 +1,105 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+                 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package server
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cloudflare/cfssl/api"
+	"github.com/cloudflare/cfssl/log"
+
+	"github.com/hyperledger/fabric-cop/idp"
+	"github.com/hyperledger/fabric-cop/util"
+)
+
+// NewRevokeHandler is constructor for revoke handler
+func NewRevokeHandler() (h http.Handler, err error) {
+	// NewHandler is constructor for register handler
+	return &api.HTTPHandler{
+		Handler: &revokeHandler{},
+		Methods: []string{"POST"}}, nil
+}
+
+// revokeHandler for revoke requests
+type revokeHandler struct {
+}
+
+// Handle an revoke request
+func (h *revokeHandler) Handle(w http.ResponseWriter, r *http.Request) error {
+
+	log.Debug("Revoke request received")
+
+	authHdr := r.Header.Get("authorization")
+	if authHdr == "" {
+		return authErr(w, errors.New("no authorization header"))
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return badRequest(w, err)
+	}
+	r.Body.Close()
+
+	cert, err := util.VerifyToken(authHdr, body)
+	if err != nil {
+		return authErr(w, err)
+	}
+
+	err = userHasAttribute(cert.Subject.CommonName, "hf.Revoker")
+	if err != nil {
+		return authErr(w, err)
+	}
+
+	// Parse revoke request body
+	var req idp.RevocationRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		return badRequest(w, err)
+	}
+
+	log.Debugf("Revoke request: %+v", req)
+
+	if req.Serial != "" && req.AKI != "" {
+		err = CFG.certDBAccessor.RevokeCertificate(req.Serial, req.AKI, req.Reason)
+		if err != nil {
+			log.Warningf("Revoke failed: %s", err)
+			return notFound(w, err)
+		}
+	} else if req.Name != "" {
+		user, err := CFG.DBAccessor.GetUser(req.Name)
+		if err != nil {
+			log.Warningf("Revoke failed: %s", err)
+			return notFound(w, err)
+		}
+		user.State = -1
+		err = CFG.DBAccessor.UpdateUser(user)
+		if err != nil {
+			log.Warningf("Revoke failed: %s", err)
+			return dbErr(w, err)
+		}
+	} else {
+		return badRequest(w, errors.New("either Name or Serial and AKI are required for a revoke request"))
+	}
+
+	log.Debug("Revoke was successful: %+v", req)
+
+	result := map[string]string{}
+	return api.SendResponse(w, result)
+}

cli/server/server.go
@@ -21,6 +21,7 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/cloudflare/cfssl/certdb/sql"
 	"github.com/cloudflare/cfssl/cli"
 	"github.com/cloudflare/cfssl/cli/serve"
 	"github.com/cloudflare/cfssl/cli/sign"
@@ -57,7 +58,7 @@ type Server struct {
 }
 
 // Command defines the command will start the server and registers endpoints
-func Command() {
+func Command() error {
 	// The server commands
 	cmds := map[string]*cli.Command{
 		"init":  InitServerCommand,
@@ -71,12 +72,10 @@ func Command() {
 	serve.SetEndpoint("enroll", NewEnrollHandler)
 	// Add the "reenroll" route/endpoint
 	serve.SetEndpoint("reenroll", NewReenrollHandler)
+	// Add the "revoke" route/endpoint
+	serve.SetEndpoint("revoke", NewRevokeHandler)
 
-	// If the CLI returns an error, exit with an appropriate status code.
-	err := cli.Start(cmds)
-	if err != nil {
-		os.Exit(1)
-	}
+	return cli.Start(cmds)
 }
 
 // CreateHome will create a home directory if it does not exist
@@ -143,6 +142,7 @@ func startMain(args []string, c cli.Config) error {
 	cfg.DB = db
 	cfg.DBAccessor = NewDBAccessor()
 	cfg.DBAccessor.SetDB(db)
+	cfg.certDBAccessor = sql.NewAccessor(db)
 
 	var cfsslCfg cli.Config
 	cfsslCfg.CAFile = cfg.CACert