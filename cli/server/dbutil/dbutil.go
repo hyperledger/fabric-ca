hyperledger-gerrit-archive/fabric-cop

Abstract DB and enable plugging in LDAP
Implement a factory to abstract DB and hide the
SQL dependency and allow for LDAP to be added.
If no database is specified, it will default to using
SQLite.

https://jira.hyperledger.org/browse/FAB-1144

Change-Id: I9b94cc9e10e82885ad4bfa63955652ac6b67339c
Signed-off-by: Saad Karim <skarim@us.ibm.com>

cli/server/dbutil/dbutil.go
@@ -29,18 +29,18 @@ import (
 )
 
 // GetDB returns a pointer to the database
-func GetDB(home string, dbdriver string, datasource string) (*sqlx.DB, error) {
+func GetDB(dbdriver string, datasource string) (*sqlx.DB, error) {
 	log.Debugf("Check if database exists: %s", datasource)
 	var err error
 	var db *sqlx.DB
 
 	switch dbdriver {
 	case "sqlite3":
-		db, err = sqlitedb(home, datasource)
+		db, _, err = NewUserRegistrySQLLite3(datasource)
 	case "postgres":
-		db, err = postgres(datasource)
+		db, _, err = NewUserRegistryPostgres(datasource)
 	case "mysql":
-		db, err = mysql(datasource)
+		db, _, err = NewUserRegistryMySQL(datasource)
 	default:
 		log.Error("Unsupported database type")
 		return nil, cop.NewError(cop.DatabaseError, "Unsupported database type")
@@ -53,32 +53,35 @@ func GetDB(home string, dbdriver string, datasource string) (*sqlx.DB, error) {
 	return db, nil
 }
 
-// sqlitedb returns a pointer to a sqlite database
-func sqlitedb(home string, datasource string) (*sqlx.DB, error) {
-	log.Debugf("Using sqlite database, connect to database in home (%s) directory", home)
-	datasource = filepath.Join(home, datasource)
+// NewUserRegistrySQLLite3 returns a pointer to a sqlite database
+func NewUserRegistrySQLLite3(datasource string) (*sqlx.DB, bool, error) {
+	log.Debugf("Using sqlite database, connect to database in home (%s) directory", datasource)
+	datasource = filepath.Join(datasource)
+	var exists bool
 
 	if datasource != "" {
 		// Check if database exists if not create it and bootstrap it based on the config file
 		if _, err := os.Stat(datasource); err != nil {
 			if os.IsNotExist(err) {
 				log.Debugf("Database (%s) does not exist", datasource)
+				exists = false
 				err2 := createSQLiteDBTables(datasource)
 				if err2 != nil {
-					return nil, err2
+					return nil, false, err2
 				}
 			} else {
 				log.Debug("Database (%s) exists", datasource)
+				exists = true
 			}
 		}
 	}
 
 	db, err := sqlx.Open("sqlite3", datasource)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
-	return db, nil
+	return db, exists, nil
 }
 
 func createSQLiteDBTables(datasource string) error {
@@ -89,11 +92,11 @@ func createSQLiteDBTables(datasource string) error {
 		return cop.WrapError(err, cop.DatabaseError, "Failed to connect to database")
 	}
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS users (id VARCHAR(64), enrollment_id VARCHAR(100), token BLOB, type VARCHAR(64), metadata VARCHAR(256), state INTEGER, serial_number bytea)"); err != nil {
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS users (id VARCHAR(64), token bytea, type VARCHAR(64), attributes VARCHAR(256), state INTEGER, serial_number bytea, authority_key_identifier bytea)"); err != nil {
 		return err
 	}
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS groups (name VARCHAR(64), parent_id VARCHAR(64))"); err != nil {
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS groups (name VARCHAR(64), parent_id VARCHAR(64), key VARCHAR(48))"); err != nil {
 		return err
 	}
 
@@ -104,10 +107,11 @@ func createSQLiteDBTables(datasource string) error {
 	return nil
 }
 
-// Postgres checks if Postgres database exists
-func postgres(datasource string) (*sqlx.DB, error) {
+// NewUserRegistryPostgres opens a connecton to a postgres database
+func NewUserRegistryPostgres(datasource string) (*sqlx.DB, bool, error) {
 	log.Debugf("Using postgres database, connecting to database...")
 
+	var exists bool
 	dbName := getDBName(datasource)
 	log.Debug("Database Name: ", dbName)
 
@@ -117,7 +121,7 @@ func postgres(datasource string) (*sqlx.DB, error) {
 	if err != nil {
 		msg := "Failed to connect to database"
 		log.Error(msg)
-		return nil, cop.WrapError(err, cop.DatabaseError, msg)
+		return nil, false, cop.WrapError(err, cop.DatabaseError, msg)
 	}
 
 	// Check if database exists
@@ -125,26 +129,28 @@ func postgres(datasource string) (*sqlx.DB, error) {
 	if err2 != nil {
 		msg := "Failed to query 'pg_database' table"
 		log.Error(msg+" error: ", err2)
-		return nil, cop.WrapError(err, cop.DatabaseError, msg)
+		return nil, false, cop.WrapError(err, cop.DatabaseError, msg)
 	}
 
 	found, _ := r.RowsAffected()
 	if found == 0 {
 		log.Debugf("Database (%s) does not exist", dbName)
+		exists = false
 		err = createPostgresDBTables(datasource, dbName, db)
 		if err != nil {
-			return nil, err
+			return nil, false, err
 		}
 	} else {
-		log.Debug("Database (%s) exists", datasource)
+		log.Debugf("Database (%s) exists", dbName)
+		exists = true
 	}
 
 	db, err = sqlx.Open("postgres", datasource)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
-	return db, nil
+	return db, exists, nil
 }
 
 // createPostgresDB creates postgres database
@@ -163,10 +169,10 @@ func createPostgresDBTables(datasource string, dbName string, db *sqlx.DB) error
 	}
 
 	log.Debug("Create Tables...")
-	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64), enrollment_id VARCHAR(100), token bytea, type VARCHAR(64), metadata VARCHAR(256), state INTEGER, serial_number bytea)"); err != nil {
+	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64), token bytea, type VARCHAR(64), attributes VARCHAR(256), state INTEGER, serial_number bytea, authority_key_identifier bytea)"); err != nil {
 		return err
 	}
-	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64))"); err != nil {
+	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64), key VARCHAR(48))"); err != nil {
 		return err
 	}
 	if _, err := database.Exec("CREATE TABLE certificates (serial_number bytea NOT NULL, authority_key_identifier bytea NOT NULL, ca_label bytea, status bytea NOT NULL, reason int, expiry timestamp, revoked_at timestamp, pem bytea NOT NULL, PRIMARY KEY(serial_number, authority_key_identifier))"); err != nil {
@@ -175,9 +181,11 @@ func createPostgresDBTables(datasource string, dbName string, db *sqlx.DB) error
 	return nil
 }
 
-func mysql(datasource string) (*sqlx.DB, error) {
+// NewUserRegistryMySQL opens a connecton to a postgres database
+func NewUserRegistryMySQL(datasource string) (*sqlx.DB, bool, error) {
 	log.Debugf("Using MySQL database, connecting to database...")
 
+	var exists bool
 	dbName := getDBName(datasource)
 	connStr := strings.Split(datasource, "/")[0] + "/"
 
@@ -185,7 +193,7 @@ func mysql(datasource string) (*sqlx.DB, error) {
 	if err != nil {
 		msg := "Failed to open to database"
 		log.Error(msg+" error: ", err)
-		return nil, cop.WrapError(err, cop.DatabaseError, msg)
+		return nil, false, cop.WrapError(err, cop.DatabaseError, msg)
 	}
 
 	// Check if database exists
@@ -195,10 +203,11 @@ func mysql(datasource string) (*sqlx.DB, error) {
 	if err != nil {
 		if err == sql.ErrNoRows {
 			log.Debugf("Database (%s) does not exist", dbName)
+			exists = false
 		} else {
 			msg := "Failed to query 'INFORMATION_SCHEMA.SCHEMATA' table"
 			log.Error(msg+" error: ", err)
-			return nil, cop.WrapError(err, cop.DatabaseError, "Failed to query 'INFORMATION_SCHEMA.SCHEMATA table")
+			return nil, false, cop.WrapError(err, cop.DatabaseError, "Failed to query 'INFORMATION_SCHEMA.SCHEMATA table")
 		}
 	}
 
@@ -206,14 +215,15 @@ func mysql(datasource string) (*sqlx.DB, error) {
 		createMySQLTables(datasource, dbName, db)
 	} else {
 		log.Debugf("Database (%s) exists", dbName)
+		exists = true
 	}
 
 	db, err = sqlx.Open("mysql", datasource)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
-	return db, nil
+	return db, exists, nil
 }
 
 func createMySQLTables(datasource string, dbName string, db *sqlx.DB) error {
@@ -229,10 +239,10 @@ func createMySQLTables(datasource string, dbName string, db *sqlx.DB) error {
 		log.Errorf("Failed to open database (%s), err: %s", dbName, err)
 	}
 	log.Debug("Create Tables...")
-	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64) NOT NULL, enrollment_id VARCHAR(100), token blob, type VARCHAR(64), metadata VARCHAR(256), state INTEGER, serial_number blob, PRIMARY KEY (id))"); err != nil {
+	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64) NOT NULL, token blob, type VARCHAR(64), attributes VARCHAR(256), state INTEGER, serial_number varbinary(20), authority_key_identifier varbinary(128), PRIMARY KEY (id))"); err != nil {
 		return err
 	}
-	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64))"); err != nil {
+	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64), key VARCHAR(48))"); err != nil {
 		return err
 	}
 	if _, err := database.Exec("CREATE TABLE certificates (serial_number varbinary(20) NOT NULL, authority_key_identifier varbinary(128) NOT NULL, ca_label varbinary(128), status varbinary(128) NOT NULL, reason int, expiry timestamp DEFAULT '1970-01-01 00:00:01', revoked_at timestamp DEFAULT '1970-01-01 00:00:01', pem varbinary(4096) NOT NULL, PRIMARY KEY(serial_number, authority_key_identifier))"); err != nil {

hyperledger/fabric-ca

cli/server/dbutil/dbutil.go
@@ -29,18 +29,18 @@ import (
 )
 
 // GetDB returns a pointer to the database
-func GetDB(home string, dbdriver string, datasource string) (*sqlx.DB, error) {
+func GetDB(dbdriver string, datasource string) (*sqlx.DB, error) {
 	log.Debugf("Check if database exists: %s", datasource)
 	var err error
 	var db *sqlx.DB
 
 	switch dbdriver {
 	case "sqlite3":
-		db, err = sqlitedb(home, datasource)
+		db, _, err = NewUserRegistrySQLLite3(datasource)
 	case "postgres":
-		db, err = postgres(datasource)
+		db, _, err = NewUserRegistryPostgres(datasource)
 	case "mysql":
-		db, err = mysql(datasource)
+		db, _, err = NewUserRegistryMySQL(datasource)
 	default:
 		log.Error("Unsupported database type")
 		return nil, cop.NewError(cop.DatabaseError, "Unsupported database type")
@@ -53,32 +53,35 @@ func GetDB(home string, dbdriver string, datasource string) (*sqlx.DB, error) {
 	return db, nil
 }
 
-// sqlitedb returns a pointer to a sqlite database
-func sqlitedb(home string, datasource string) (*sqlx.DB, error) {
-	log.Debugf("Using sqlite database, connect to database in home (%s) directory", home)
-	datasource = filepath.Join(home, datasource)
+// NewUserRegistrySQLLite3 returns a pointer to a sqlite database
+func NewUserRegistrySQLLite3(datasource string) (*sqlx.DB, bool, error) {
+	log.Debugf("Using sqlite database, connect to database in home (%s) directory", datasource)
+	datasource = filepath.Join(datasource)
+	var exists bool
 
 	if datasource != "" {
 		// Check if database exists if not create it and bootstrap it based on the config file
 		if _, err := os.Stat(datasource); err != nil {
 			if os.IsNotExist(err) {
 				log.Debugf("Database (%s) does not exist", datasource)
+				exists = false
 				err2 := createSQLiteDBTables(datasource)
 				if err2 != nil {
-					return nil, err2
+					return nil, false, err2
 				}
 			} else {
 				log.Debug("Database (%s) exists", datasource)
+				exists = true
 			}
 		}
 	}
 
 	db, err := sqlx.Open("sqlite3", datasource)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
-	return db, nil
+	return db, exists, nil
 }
 
 func createSQLiteDBTables(datasource string) error {
@@ -89,11 +92,11 @@ func createSQLiteDBTables(datasource string) error {
 		return cop.WrapError(err, cop.DatabaseError, "Failed to connect to database")
 	}
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS users (id VARCHAR(64), enrollment_id VARCHAR(100), token BLOB, type VARCHAR(64), metadata VARCHAR(256), state INTEGER, serial_number bytea)"); err != nil {
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS users (id VARCHAR(64), token bytea, type VARCHAR(64), attributes VARCHAR(256), state INTEGER, serial_number bytea, authority_key_identifier bytea)"); err != nil {
 		return err
 	}
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS groups (name VARCHAR(64), parent_id VARCHAR(64))"); err != nil {
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS groups (name VARCHAR(64), parent_id VARCHAR(64), group_key VARCHAR(48))"); err != nil {
 		return err
 	}
 
@@ -104,10 +107,11 @@ func createSQLiteDBTables(datasource string) error {
 	return nil
 }
 
-// Postgres checks if Postgres database exists
-func postgres(datasource string) (*sqlx.DB, error) {
+// NewUserRegistryPostgres opens a connecton to a postgres database
+func NewUserRegistryPostgres(datasource string) (*sqlx.DB, bool, error) {
 	log.Debugf("Using postgres database, connecting to database...")
 
+	var exists bool
 	dbName := getDBName(datasource)
 	log.Debug("Database Name: ", dbName)
 
@@ -117,7 +121,7 @@ func postgres(datasource string) (*sqlx.DB, error) {
 	if err != nil {
 		msg := "Failed to connect to database"
 		log.Error(msg)
-		return nil, cop.WrapError(err, cop.DatabaseError, msg)
+		return nil, false, cop.WrapError(err, cop.DatabaseError, msg)
 	}
 
 	// Check if database exists
@@ -125,26 +129,28 @@ func postgres(datasource string) (*sqlx.DB, error) {
 	if err2 != nil {
 		msg := "Failed to query 'pg_database' table"
 		log.Error(msg+" error: ", err2)
-		return nil, cop.WrapError(err, cop.DatabaseError, msg)
+		return nil, false, cop.WrapError(err, cop.DatabaseError, msg)
 	}
 
 	found, _ := r.RowsAffected()
 	if found == 0 {
 		log.Debugf("Database (%s) does not exist", dbName)
+		exists = false
 		err = createPostgresDBTables(datasource, dbName, db)
 		if err != nil {
-			return nil, err
+			return nil, false, err
 		}
 	} else {
-		log.Debug("Database (%s) exists", datasource)
+		log.Debugf("Database (%s) exists", dbName)
+		exists = true
 	}
 
 	db, err = sqlx.Open("postgres", datasource)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
-	return db, nil
+	return db, exists, nil
 }
 
 // createPostgresDB creates postgres database
@@ -163,21 +169,27 @@ func createPostgresDBTables(datasource string, dbName string, db *sqlx.DB) error
 	}
 
 	log.Debug("Create Tables...")
-	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64), enrollment_id VARCHAR(100), token bytea, type VARCHAR(64), metadata VARCHAR(256), state INTEGER, serial_number bytea)"); err != nil {
+	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64), token bytea, type VARCHAR(64), attributes VARCHAR(256), state INTEGER, serial_number bytea, authority_key_identifier bytea)"); err != nil {
+		log.Errorf("Error creating users table [error: %s] ", err)
+
 		return err
 	}
-	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64))"); err != nil {
+	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64), group_key VARCHAR(48))"); err != nil {
+		log.Errorf("Error creating groups table [error: %s] ", err)
 		return err
 	}
 	if _, err := database.Exec("CREATE TABLE certificates (serial_number bytea NOT NULL, authority_key_identifier bytea NOT NULL, ca_label bytea, status bytea NOT NULL, reason int, expiry timestamp, revoked_at timestamp, pem bytea NOT NULL, PRIMARY KEY(serial_number, authority_key_identifier))"); err != nil {
+		log.Errorf("Error creating certificates table [error: %s] ", err)
 		return err
 	}
 	return nil
 }
 
-func mysql(datasource string) (*sqlx.DB, error) {
+// NewUserRegistryMySQL opens a connecton to a postgres database
+func NewUserRegistryMySQL(datasource string) (*sqlx.DB, bool, error) {
 	log.Debugf("Using MySQL database, connecting to database...")
 
+	var exists bool
 	dbName := getDBName(datasource)
 	connStr := strings.Split(datasource, "/")[0] + "/"
 
@@ -185,7 +197,7 @@ func mysql(datasource string) (*sqlx.DB, error) {
 	if err != nil {
 		msg := "Failed to open to database"
 		log.Error(msg+" error: ", err)
-		return nil, cop.WrapError(err, cop.DatabaseError, msg)
+		return nil, false, cop.WrapError(err, cop.DatabaseError, msg)
 	}
 
 	// Check if database exists
@@ -195,10 +207,11 @@ func mysql(datasource string) (*sqlx.DB, error) {
 	if err != nil {
 		if err == sql.ErrNoRows {
 			log.Debugf("Database (%s) does not exist", dbName)
+			exists = false
 		} else {
 			msg := "Failed to query 'INFORMATION_SCHEMA.SCHEMATA' table"
 			log.Error(msg+" error: ", err)
-			return nil, cop.WrapError(err, cop.DatabaseError, "Failed to query 'INFORMATION_SCHEMA.SCHEMATA table")
+			return nil, false, cop.WrapError(err, cop.DatabaseError, "Failed to query 'INFORMATION_SCHEMA.SCHEMATA table")
 		}
 	}
 
@@ -206,14 +219,15 @@ func mysql(datasource string) (*sqlx.DB, error) {
 		createMySQLTables(datasource, dbName, db)
 	} else {
 		log.Debugf("Database (%s) exists", dbName)
+		exists = true
 	}
 
 	db, err = sqlx.Open("mysql", datasource)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
 
-	return db, nil
+	return db, exists, nil
 }
 
 func createMySQLTables(datasource string, dbName string, db *sqlx.DB) error {
@@ -229,13 +243,16 @@ func createMySQLTables(datasource string, dbName string, db *sqlx.DB) error {
 		log.Errorf("Failed to open database (%s), err: %s", dbName, err)
 	}
 	log.Debug("Create Tables...")
-	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64) NOT NULL, enrollment_id VARCHAR(100), token blob, type VARCHAR(64), metadata VARCHAR(256), state INTEGER, serial_number blob, PRIMARY KEY (id))"); err != nil {
+	if _, err := database.Exec("CREATE TABLE users (id VARCHAR(64) NOT NULL, token blob, type VARCHAR(64), attributes VARCHAR(256), state INTEGER, serial_number varbinary(20), authority_key_identifier varbinary(128), PRIMARY KEY (id))"); err != nil {
+		log.Errorf("Error creating users table [error: %s] ", err)
 		return err
 	}
-	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64))"); err != nil {
+	if _, err := database.Exec("CREATE TABLE groups (name VARCHAR(64), parent_id VARCHAR(64), group_key VARCHAR(48))"); err != nil {
+		log.Errorf("Error creating groups table [error: %s] ", err)
 		return err
 	}
 	if _, err := database.Exec("CREATE TABLE certificates (serial_number varbinary(20) NOT NULL, authority_key_identifier varbinary(128) NOT NULL, ca_label varbinary(128), status varbinary(128) NOT NULL, reason int, expiry timestamp DEFAULT '1970-01-01 00:00:01', revoked_at timestamp DEFAULT '1970-01-01 00:00:01', pem varbinary(4096) NOT NULL, PRIMARY KEY(serial_number, authority_key_identifier))"); err != nil {
+		log.Errorf("Error creating certificates table [error: %s] ", err)
 		return err
 	}
 