ed2ad83fe8636b411657d68bb1aa7163f1c5f493
ASHUTOSH KUMAR <ashutosh_kumar@hotmail.com>
2016-12-13 21:52:45 -0500

    Crypto Support for TCert
    This is the 1st change set to support tcerts in the COP server.
    In particular, this change set contains a few utility functions which
    will be used by a follow up change set.
    
    See https://jira.hyperledger.org/browse/FAB-873
    
    Change-Id: I18e2b32fd344844a4fa2158a7c1352d735902295
    Signed-off-by: ASHUTOSH KUMAR <ashutosh_kumar@hotmail.com>
    Signed-off-by: Keith Smith <bksmith@us.ibm.com>

---
+++ lib/tcert/util_test.go
@@ -0,0 +1,120 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tcert
+
+import (
+	"io/ioutil"
+	"math/big"
+	"testing"
+
+	"crypto/rand"
+)
+
+func TestGenNumber(t *testing.T) {
+	num := GenNumber(big.NewInt(20))
+	if num == nil {
+		t.Fatalf("Failed in GenNumber")
+	}
+}
+
+func TestECCertificate(t *testing.T) {
+	publicKeyBuff, err := ioutil.ReadFile("../../testdata/ec.pem")
+	if err != nil {
+		t.Fatalf("Cannot read EC Certificate from file system")
+	}
+	_, error := GetCertificate(publicKeyBuff)
+	if error != nil {
+		t.Fatalf("Cannot create EC Certificate \t [%v]", error)
+	}
+}
+
+func TestCBCPKCS7EncryptCBCPKCS7Decrypt(t *testing.T) {
+
+	// Note: The purpose of this test is not to test AES-256 in CBC mode's strength
+	// ... but rather to verify the code wrapping/unwrapping the cipher.
+	key := make([]byte, AESKeyLength)
+	rand.Reader.Read(key)
+
+	var ptext = []byte("a message with arbitrary length (42 bytes)")
+
+	encrypted, encErr := CBCPKCS7Encrypt(key, ptext)
+	if encErr != nil {
+		t.Fatalf("Error encrypting '%s': %s", ptext, encErr)
+	}
+
+	decrypted, dErr := CBCPKCS7Decrypt(key, encrypted)
+	if dErr != nil {
+		t.Fatalf("Error decrypting the encrypted '%s': %v", ptext, dErr)
+	}
+
+	if string(ptext[:]) != string(decrypted[:]) {
+		t.Fatal("Decrypt( Encrypt( ptext ) ) != ptext: Ciphertext decryption with the same key must result in the original plaintext!")
+	}
+
+}
+
+func TestPreKey(t *testing.T) {
+	rootKey := CreateRootPreKey()
+	if len(rootKey) == 0 {
+		t.Fatal("Root Key Cannot be generated")
+	}
+
+}
+
+func TestSerialNumber(t *testing.T) {
+	publicKeyBuff, err := ioutil.ReadFile("../../testdata/ec.pem")
+	if err != nil {
+		t.Fatalf("Cannot read EC Certificate from file system")
+	}
+	_, error := GetCertitificateSerialNumber(publicKeyBuff)
+
+	if error != nil {
+		t.Fatalf("Cannot create EC Certificate \t [%v]", error)
+	}
+
+}
+
+func TestGetBadCertificate(t *testing.T) {
+	buf, err := ioutil.ReadFile("../../testdata/cop.json")
+	if err != nil {
+		t.Fatalf("Cannot read certificate from file system")
+	}
+
+	_, err = GetCertificate([]byte(buf))
+	if err == nil {
+		t.Fatalf("Should have failed since file is json:\t [%v] ", err)
+	}
+}
+
+func TestGenerateUUID(t *testing.T) {
+	_, err := GenerateIntUUID()
+	if err != nil {
+		t.Errorf("GenerateIntUUID failed: %s", err)
+	}
+}
+
+func TestDerToPem(t *testing.T) {
+
+	buf, err := ioutil.ReadFile("../../testdata/ecTest.der")
+	if err != nil {
+		t.Fatalf("Cannot read Certificate in DER format: %s", err)
+	}
+	cert := ConvertDERToPEM(buf, "CERTIFICATE")
+	if cert == nil {
+		t.Fatalf("Failed to ConvertDERToPEM")
+	}
+}
