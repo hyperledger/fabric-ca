hyperledger-gerrit-archive/fabric-cop

COP UserRegistry Consolidation
Remove UpdateField/GetField and replace with a single
GetUser and UpdateUser call. This will remove the need
to call both UpdateUser and UpdateField when updating a
field that was not initially part of the UserInfo struct
that was defined in the SPI. The UserInfo struct has been
expanded to support more fields.

https://jira.hyperledger.org/browse/FAB-1442

Change-Id: I1181a54a99bea42f4e6688f55c968798acec8025
Signed-off-by: Saad Karim <skarim@us.ibm.com>

cli/server/spi/userregistry.go
@@ -25,11 +25,13 @@ import "github.com/hyperledger/fabric-cop/idp"
 
 // UserInfo contains information about a user
 type UserInfo struct {
-	Name       string
-	Pass       string
-	Type       string
-	Group      string
-	Attributes []idp.Attribute
+	Name           string
+	Pass           string
+	Type           string
+	Group          string
+	Attributes     []idp.Attribute
+	State          int
+	MaxEnrollments int
 }
 
 // GroupInfo defines a group name and its parent
@@ -63,8 +65,6 @@ type UserRegistry interface {
 	InsertUser(user UserInfo) error
 	UpdateUser(user UserInfo) error
 	DeleteUser(id string) error
-	UpdateField(id string, field int, value interface{}) error
-	GetField(id string, field int) (interface{}, error)
 	GetGroup(name string) (Group, error)
 	GetRootGroup() (Group, error)
 	InsertGroup(name string, parentID string) error

hyperledger/fabric-ca

cli/server/spi/userregistry.go
@@ -25,11 +25,13 @@ import "github.com/hyperledger/fabric-cop/api"
 
 // UserInfo contains information about a user
 type UserInfo struct {
-	Name       string
-	Pass       string
-	Type       string
-	Group      string
-	Attributes []api.Attribute
+	Name           string
+	Pass           string
+	Type           string
+	Group          string
+	Attributes     []api.Attribute
+	State          int
+	MaxEnrollments int
 }
 
 // GroupInfo defines a group name and its parent
@@ -60,11 +62,10 @@ type Group interface {
 // UserRegistry is the API for retreiving users and groups
 type UserRegistry interface {
 	GetUser(id string, attrs []string) (User, error)
+	GetUserInfo(id string) (UserInfo, error)
 	InsertUser(user UserInfo) error
 	UpdateUser(user UserInfo) error
 	DeleteUser(id string) error
-	UpdateField(id string, field int, value interface{}) error
-	GetField(id string, field int) (interface{}, error)
 	GetGroup(name string) (Group, error)
 	GetRootGroup() (Group, error)
 	InsertGroup(name string, parentID string) error