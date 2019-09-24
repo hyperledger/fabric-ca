eab527aad7b440fd106259f55612f4cfb20cd3cd
Brett Logan <Brett.T.Logan@ibm.com>
2019-09-24 14:14:13 -0400

    [FABCI-419] Version Go Tools
    Use the tools.go model to version tooling deps
    
    Signed-off-by: Brett Logan <Brett.T.Logan@ibm.com>

---
+++ gotools.mk
@@ -0,0 +1,15 @@
+# Copyright the Hyperledger Fabric contributors. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+GOTOOLS = gocov gocov-xml goimports golint
+
+.PHONY: gotools
+gotools: $(patsubst %,build/tools/%, $(GOTOOLS))
+
+build/tools/%: tools/go.mod tools/tools.go
+	@mkdir -p $(@D)
+	@$(eval TOOL = ${subst build/tools/,,${@}})
+	@$(eval FQP = $(shell grep ${TOOL} tools/tools.go | cut -d " " -f2 | grep ${TOOL}\"$))
+	@echo Installing ${TOOL} at ${CURDIR}/$(TOOLS) from ${FQP}
+	@cd tools && GO111MODULE=on GOBIN=${CURDIR}/$(TOOLS) go install ${FQP}
---
+++ tools/go.mod
@@ -0,0 +1,10 @@
+module tools
+
+go 1.12
+
+require (
+	github.com/AlekSi/gocov-xml v0.0.0-20190121064608-3a14fb1c4737
+	github.com/axw/gocov v1.0.0
+	golang.org/x/lint v0.0.0-20190909230951-414d861bb4ac
+	golang.org/x/tools v0.0.0-20190924052046-3ac2a5bbd98a
+)
