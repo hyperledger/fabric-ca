ea63131687f71924be920f12ae9e1ee8b57461f4
Brett Logan <Brett.T.Logan@ibm.com>
2019-10-15 01:31:37 -0400

    [FABC-882] Remove Fabric BaseImage
    Replaces the use of the Fabric baseimage with a
    raw golang container for building the fabric binaries
    and debain:buster for the FVT test suite.
    
    Fabric baseimage is a bloated, specialty image
    that was historically used by Fabric with all
    of dependent utilies require by fabric baked into it.
    In the case of the CA FVT container, the only piece
    we gain by using base image is having openssl already
    installed in the image.
    
    Signed-off-by: Brett Logan <Brett.T.Logan@ibm.com>

--- images/fabric-ca-fvt/payload/system_update.sh
+++ images/fabric-ca-fvt/payload/system_update.sh
@@ -17,12 +17,11 @@ printf "LANG=en_US.UTF-8\nLANGUAGE=en_US.UTF-8\n" > /etc/default/locale
 dpkg-reconfigure locales && update-locale LANG=en_US.UTF-8 || let RC+=1
 
 # Install more test depedencies
-echo "mysql-server mysql-server/root_password password mysql" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password mysql" | debconf-set-selections
+
 apt-get -y install --no-install-recommends rsyslog bc vim lsof sqlite3 haproxy postgresql-$PGVER \
            postgresql-client-common postgresql-contrib-$PGVER isag jq git html2text \
            debconf-utils zsh htop python2.7-minimal libpython2.7-stdlib \
-           mysql-client  mysql-common mysql-server parallel || let RC+=1
+           parallel netcat wget lsb-release gnupg ca-certificates || let RC+=1
 apt-get -y install ssl-cert || let RC+=1
 apt-get -y autoremove
 
