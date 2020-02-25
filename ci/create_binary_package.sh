5616f18a389d76c11ffea6e3b71f29fc37d081f4
Brett Logan <brett.t.logan@ibm.com>
2020-02-25 18:13:04 -0500

    Update release make targets
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- ci/create_binary_package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright IBM Corp. All Rights Reserved.
-#
-# SPDX-License-Identifier: Apache-2.0
-set -eu -o pipefail
-
-make "release/${TARGET}"
-cd "release/${TARGET}/bin"
-tar -czvf "${TARGET}-${RELEASE}.tar.gz" "fabric-ca-client"
