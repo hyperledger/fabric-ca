41d4b3f1ccefe03ba961d9f1fbd2cbd13bb995e0
Will Lahti <wtlahti@us.ibm.com>
2020-09-10 17:19:02 -0400

    Allow reenroll to reuse existing private key
    FABC-914
    
    Signed-off-by: Will Lahti <wtlahti@us.ibm.com>

--- cmd/fabric-ca-client/command/config.go
+++ cmd/fabric-ca-client/command/config.go
@@ -106,6 +106,11 @@ tls:
 #
 #  cn - Used by CAs to determine which domain the certificate is to be generated for
 #
+#  keyrequest - Properties to use when generating a private key.
+#     algo - key generation algorithm to use
+#     size - size of key to generate
+#     reusekey - reuse existing key during reenrollment
+#
 #  serialnumber - The serialnumber field, if specified, becomes part of the issued
 #     certificate's DN (Distinguished Name).  For example, one use case for this is
 #     a company with its own CA (Certificate Authority) which issues certificates
@@ -139,6 +144,7 @@ csr:
   keyrequest:
     algo: ecdsa
     size: 256
+    reusekey: false
   serialnumber:
   names:
     - C: US
