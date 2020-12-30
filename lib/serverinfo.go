50cdfc63b7e49bc075f525be56ae00c9415e716c
Matthew Sykes <sykesmat@us.ibm.com>
2020-12-30 11:35:36 -0500

    Remove dead ServerInfoResponseNet struct
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- lib/serverinfo.go
+++ lib/serverinfo.go
@@ -11,18 +11,6 @@ import (
 	"github.com/hyperledger/fabric-ca/lib/metadata"
 )
 
-// ServerInfoResponseNet is the response to the GET /cainfo request
-type ServerInfoResponseNet struct {
-	// CAName is a unique name associated with fabric-ca-server's CA
-	CAName string
-	// Base64 encoding of PEM-encoded certificate chain
-	CAChain string
-	// Base64 encoding of idemix issuer public key
-	IssuerPublicKey string
-	// Version of the server
-	Version string
-}
-
 func newCAInfoEndpoint(s *Server) *serverEndpoint {
 	return &serverEndpoint{
 		Path:    "cainfo",
